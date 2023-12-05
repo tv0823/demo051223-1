71603e42a23cfaafc1a3f703eac5057e6cfa337e
tv0823 <tv0823@bs.amalnet.k12.il>
2023-12-05 08:50:30 +0200

    click me

--- app/src/main/java/com/example/demo051223_1/MainActivity.java
+++ app/src/main/java/com/example/demo051223_1/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.demo051223_1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void hello(View view) {
     }
 }
